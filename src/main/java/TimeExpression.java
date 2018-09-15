84f9ab2cdfbd7fceeb842dc23a2cbca4f2a64c52
emystein <emystein@gmail.com>
2018-09-15 13:07:06 -0300

    Formatted TimeExpression

--- src/main/java/TimeExpression.java
+++ src/main/java/TimeExpression.java
@@ -16,27 +16,30 @@ public class TimeExpression {
 	}
 
 	public static TimeExpression dailyEveryFromOnwards(int anAmountOfDays, LocalDate startDate) {
-//		return new RecurringTimeExpression(startDate, new SpecificDateOccurrence(), new OccurrenceAdvance(anAmountOfDays, new DailyAdvanceCommand()));
-		return new RecurringTimeExpression(new SpecificDateOccurrence(), new EventRecurrence(startDate, new DailyPeriodLength(), anAmountOfDays));
+		return new RecurringTimeExpression(new SpecificDateOccurrence(),
+				new EventRecurrence(startDate, new DailyPeriodLength(), anAmountOfDays));
 	}
 
 	public static TimeExpression monthlyEveryOnFromOnwards(int anAmountOfMonths, int aDayInMonth, YearMonth yearMonth) {
 		EventOccurrence eventOccurrence = new DayOfMonthOccurrence(aDayInMonth);
 		LocalDate startDate = eventOccurrence.forDate(yearMonth.atDay(1));
-//		return new RecurringTimeExpression(startDate, eventOccurrence, new OccurrenceAdvance(anAmountOfMonths, new MonthlyAdvanceCommand()));
-		return new RecurringTimeExpression(eventOccurrence, new EventRecurrence(startDate, new MonthlyPeriodLength(), anAmountOfMonths));
+
+		return new RecurringTimeExpression(eventOccurrence,
+				new EventRecurrence(startDate, new MonthlyPeriodLength(), anAmountOfMonths));
 	}
 
 	public static TimeExpression monthlyEveryOnOfFromOnwards(int anAmountOfMonths, YearMonth yearMonth, EventOccurrence eventOccurrence) {
 		LocalDate startDate = eventOccurrence.forDate(yearMonth.atDay(1));
-//		return new RecurringTimeExpression(startDate, eventOccurrence, new OccurrenceAdvance(anAmountOfMonths, new MonthlyAdvanceCommand()));
-		return new RecurringTimeExpression(eventOccurrence, new EventRecurrence(startDate, new MonthlyPeriodLength(), anAmountOfMonths));
+
+		return new RecurringTimeExpression(eventOccurrence,
+				new EventRecurrence(startDate, new MonthlyPeriodLength(), anAmountOfMonths));
 	}
 
 	public static TimeExpression yearlyEveryOnFromOnwards(int anAmountOfYears, MonthDay aMonthDay, int anYear) {
 		LocalDate startDate = LocalDate.of(anYear, aMonthDay.getMonth(), aMonthDay.getDayOfMonth());
-//		return new RecurringTimeExpression(startDate, new SpecificDateOccurrence(), new OccurrenceAdvance(anAmountOfYears, new YearlyAdvanceCommand()));
-		return new RecurringTimeExpression(new SpecificDateOccurrence(), new EventRecurrence(startDate, new YearlyPeriodLength(), anAmountOfYears));
+
+		return new RecurringTimeExpression(new SpecificDateOccurrence(),
+				new EventRecurrence(startDate, new YearlyPeriodLength(), anAmountOfYears));
 	}
 
 	public boolean isRecurringOn(LocalDate aDate) {
